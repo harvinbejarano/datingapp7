1608aeb0e53b07fba9ce3bdb5ace30b800a93560
Harvin Bejarano <harvybj@gmail.com>
2023-09-05 17:28:54 -0500

    initial commit

---
+++ DatingApp/Controllers/UsersController.cs
@@ -0,0 +1,36 @@
+﻿namespace DatingApp.Controllers
+{
+    using DatingApp.Data;
+    using DatingApp.Entities;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.EntityFrameworkCore;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext context;
+
+        public UsersController(DataContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            var users = await context.Users.ToListAsync();
+
+            return Ok(users);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<IEnumerable<AppUser>>> Get(int id)
+        {
+            var user = await context.Users.FindAsync(id);
+
+            return Ok(user);
+        }
+    }
+}
---
+++ DatingApp/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿namespace DatingApp.Data
+{
+    using DatingApp.Entities;
+    using Microsoft.EntityFrameworkCore;
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+
+    }
+}

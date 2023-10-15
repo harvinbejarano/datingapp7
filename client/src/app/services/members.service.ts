9ce4d4d96f6f238ac4e3de0278a8fb8c5727fe23
Harvin Bejarano <harvybj@gmail.com>
2023-10-15 12:50:28 -0500

    updating member details

--- client/src/app/services/members.service.ts
+++ client/src/app/services/members.service.ts
@@ -12,23 +12,11 @@ export class MembersService {
   constructor(private http: HttpClient) { }
 
   getMembers(){
-    return this.http.get<Member[]>(this.baseUrl+ 'users', this.getHttpoptions());
+    return this.http.get<Member[]>(this.baseUrl+ 'users');
   }
 
   getMember(username: string){
-    return this.http.get<Member[]>(this.baseUrl+ 'users/' + username, this.getHttpoptions());
+    return this.http.get<Member>(this.baseUrl+ 'users/' + username);
   }
-
-  getHttpoptions(){
-    const userString = localStorage.getItem('user');
-    if(!userString) return ;
-
-    const user = JSON.parse(userString);
-
-    return {
-      headers: new HttpHeaders({
-        Authorization: 'Bearer ' + user.token
-      })
-    }
-  }
+  
 }

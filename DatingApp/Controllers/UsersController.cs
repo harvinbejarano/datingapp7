b5bfc533495a30f44ba84773c94cf3f9abae57cb
Harvin Bejarano <harvybj@gmail.com>
2023-10-24 15:38:43 -0500

    update AddPhoto response

--- DatingApp/Controllers/UsersController.cs
+++ DatingApp/Controllers/UsersController.cs
@@ -71,7 +71,11 @@
 
             user.Photos.Add(photo);
 
-            if(await userRepository.SaveAllAsync()) return mapper.Map<PhotoDto>(photo);
+            if (await userRepository.SaveAllAsync())
+            {
+                return CreatedAtAction(nameof(GetUser), 
+                        new {username = user.UserName}, mapper.Map<PhotoDto>(photo)); 
+            }
 
             return BadRequest("Problem adding photo");
         }
